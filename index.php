4076c9d186713c15f88fdb3ffd9fd8ec105b232a
kharchenko-nikolay <ithimikat@gmail.com>
2021-01-21 13:01:44 +0200

    add project

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require_once 'public/include/header.php'; ?>
+
+<main>
+    <section>
+        <!-- Если буду делать статьи оборачивать каждую статью тегом article и внутри обязательно заголовок h2 | h3 -->
+    </section>
+
+    <section class="workeds">
+
+    </section>
+</main>
+
+<?php require_once 'public/include/footer.php'; ?>
+
+
---
+++ public/include/header.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Прокол грунта под дорогой</title>
+    <link rel="icon" href="public/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="public/css/style.css" type="text/css">
+</head>
+<body>
+<header>
+    <img src="public/images/header.jpg" alt="header">
+    <img src="public/images/sun.png" alt="sun" class="sun-img">
+    <a href="/" class="logo-text">
+        <h1>Прокол под дорогой<br>Монтаж водопровода</h1>
+    </a>
+    <div class="phones">
+        <address style="color: #3b3b3b;">г. Днепр</address>
+        <div class="phone" style="margin-top: 5px;">
+            <img src="public/images/telecom-operators/lifecell.png" alt="Лайф">
+            <span>063 636 42 60</span>
+        </div>
+        <div class="phone">
+            <img src="public/images/telecom-operators/kievstar.png" alt="Киевстар">
+            <span>097 457 61 20</span>
+        </div>
+        <div class="phone">
+            <img src="public/images/telecom-operators/vodafone.png" alt="Водафон">
+            <span>050 238 71 22</span>
+        </div>
+        <button class="btn-call-order">Заказать звонок</button>
+    </div>
+    <nav class="main-menu">
+        <ul class="menu">
+            <li><a class="menu-item" href="">Главная</a></li>
+            <li><a class="menu-item" href="">Виды работ</a></li>
+            <li><a class="menu-item" href="">Выполненные работы</a></li>
+            <li><a class="menu-item" href="">Отзывы</a></li>
+            <li><a class="menu-item" href="">Контакты</a></li>
+            <li><a class="menu-item" href="">Цены</a></li>
+        </ul>
+    </nav>
+    <div class="carousel">
+
+    </div>
+</header>
