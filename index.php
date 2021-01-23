7c6b23dd62e7d7c3c7de418254e35765a80100c1
Kharchenko Nikolay <ithimikat@gmail.com>
2021-01-23 23:20:40 +0200

    updated section types works

--- index.php
+++ index.php
@@ -1,40 +1,52 @@
 <?php require_once 'public/include/header.php'; ?>
-<!-- Если буду делать статьи оборачивать каждую статью тегом article и внутри обязательно заголовок h2 | h3 -->
+
 <main>
     <section class="types-works">
         <div class="container-center">
             <h2>Виды выполняемых работ</h2>
             <div class="flex-container">
                 <div class="type-work">
-                    <img src="https://baltija.eu/wp-content/uploads/2020/06/dbec3d4f20c6ec37c88c60ee11ea8916.jpg" alt="">
+                    <img src="public/images/photos-of-works/prokol.jpg"
+                         alt="прокол грунта под дорогой, прокол под землей"
+                         title="Прокол под дорогой для протяжки труб">
                     <h3>Прокол грунта под дорогой</h3>
                 </div>
                 <div class="type-work">
-                    <img src="https://strojdvor.ru/wp-content/uploads/2019/04/proyektirovaniye-vodosnabzheniya-001.jpg" alt="">
-                    <h3>Монтаж наружного водопровода</h3>
+                    <img src="public/images/photos-of-works/montag-vodoprovoda.jpg"
+                         alt="монтаж труб водопровода наружных сетей"
+                         title="Монтаж водопровода">
+                    <h3>Монтаж труб наружного водопровода</h3>
                 </div>
                 <div class="type-work">
-                    <img src="https://lh3.googleusercontent.com/proxy/J5tVCc2we2KbEyeRZJ_2nnwcSoUFv0xAlsvBwgAXIugey-4mu7YERFZ2wkd0miA5KJvOl7aP1B8ANPRk9YR8BQ" alt="">
-                    <h3>Замена труб в частном секторе</h3>
+                    <img src="public/images/photos-of-works/vodoprovod-v-dome.jpg"
+                         alt="замена труб в частном доме и подвалах многоэтажек"
+                         title="Замена старых труб на новые">
+                    <h3>Замена труб в частном доме</h3>
                 </div>
                 <div class="type-work">
-                    <img src="https://domsdelat.ru/wp-content/uploads/2016/01/ustrojstvo-kanalizacii-v-chastnom-dome-svoimi-rukami.jpg" alt="">
-                    <h3>Монтаж наружной канализации</h3>
+                    <img src="public/images/photos-of-works/kanalizaciya.jpg"
+                         alt="монтаж труб наружной канализации"
+                         title="Монтаж канализации">
+                    <h3>Монтаж труб наружной канализации</h3>
                 </div>
                 <div class="type-work">
-                    <img src="https://55.img.avito.st/640x480/7990828355.jpg" alt="">
+                    <img src="public/images/photos-of-works/vrezka.jpg"
+                         alt="врезка в водопроводную трубу под давлением"
+                         title="Врезка в водопровод">
                     <h3>Врезки в водопровод под давлением</h3>
                 </div>
                 <div class="type-work">
-                    <img src="https://strojdvor.ru/wp-content/uploads/2018/09/podsoedinenie-vodoprovoda-v-deystvuyushhie-seti-1.jpg" alt="">
-                    <h3>Строительство колодцев</h3>
+                    <img src="public/images/photos-of-works/sanaciya.jpg"
+                         alt="санация трубопроводов, протяжка трубы в трубу"
+                         title="Протяжка трубы в трубу">
+                    <h3>Санация трубопроводов, протяжка трубы в трубу</h3>
                 </div>
             </div>
             <a href="" class="detail">Подробнее</a>
         </div>
     </section>
     <section class="advantages">
-        
+
     </section>
 </main>
 
