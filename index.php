954452574ede36b6de0d85d29c8fb1da85016c14
Kharchenko Nikolay <ithimikat@gmail.com>
2021-01-25 22:29:11 +0200

    added block advantages

--- index.php
+++ index.php
@@ -7,37 +7,37 @@
         <div class="container-center">
             <h2>Виды выполняемых работ</h2>
             <div class="flex-container">
-                <div class="type-work">
+                <div class="wrapper-type-work">
                     <img src="public/images/photos-of-works/prokol.jpg"
                          alt="прокол грунта под дорогой, прокол под землей"
                          title="Прокол под дорогой для протяжки труб">
                     <h3>Прокол грунта под дорогой</h3>
                 </div>
-                <div class="type-work">
+                <div class="wrapper-type-work">
                     <img src="public/images/photos-of-works/montag-vodoprovoda.jpg"
                          alt="монтаж труб водопровода наружных сетей"
                          title="Монтаж водопровода">
                     <h3>Монтаж труб наружного водопровода</h3>
                 </div>
-                <div class="type-work">
+                <div class="wrapper-type-work">
                     <img src="public/images/photos-of-works/vodoprovod-v-dome.jpg"
                          alt="замена труб в частном доме и подвалах многоэтажек"
                          title="Замена старых труб на новые">
                     <h3>Замена труб в частном доме</h3>
                 </div>
-                <div class="type-work">
+                <div class="wrapper-type-work">
                     <img src="public/images/photos-of-works/kanalizaciya.jpg"
                          alt="монтаж труб наружной канализации"
                          title="Монтаж канализации">
                     <h3>Монтаж труб наружной канализации</h3>
                 </div>
-                <div class="type-work">
+                <div class="wrapper-type-work">
                     <img src="public/images/photos-of-works/vrezka.jpg"
                          alt="врезка в водопроводную трубу под давлением"
                          title="Врезка в водопровод">
                     <h3>Врезки в водопровод под давлением</h3>
                 </div>
-                <div class="type-work">
+                <div class="wrapper-type-work">
                     <img src="public/images/photos-of-works/sanaciya.jpg"
                          alt="санация трубопроводов, протяжка трубы в трубу"
                          title="Протяжка трубы в трубу">
@@ -52,10 +52,34 @@
         <div class="container-center">
             <h2>Наши преимущества</h2>
             <div class="flex-container">
-
+                <div class="wrapper-advantage">
+                    <img src="public/images/photos-of-works/quality.png" alt="качество работ">
+                    <p>Качество работ</p>
+                </div>
+                <div class="wrapper-advantage">
+                    <img src="public/images/photos-of-works/terms.png" alt="">
+                    <p>Сроки выполнения</p>
+                </div>
+                <div class="wrapper-advantage">
+                    <img src="public/images/photos-of-works/warranty.png" alt="">
+                    <p>Гарантия</p>
+                </div>
+                <div class="wrapper-advantage">
+                    <img src="public/images/photos-of-works/master.png" alt="">
+                    <p>Выезд мастера бесплатный</p>
+                </div>
+                <div class="wrapper-advantage">
+                    <img src="public/images/photos-of-works/delivery.png" alt="">
+                    <p>Закупка материала бесплатно</p>
+                </div>
+                <div class="wrapper-advantage">
+                    <img src="public/images/photos-of-works/key.png" alt="работа под ключ">
+                    <p>Выполнение работы под ключ</p>
+                </div>
             </div>
         </div>
     </section>
+    <div class="block-separator"></div>
 </main>
 
 <?php require_once 'public/include/footer.php'; ?>
