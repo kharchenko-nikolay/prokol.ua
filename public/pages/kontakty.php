42774075ca2720656d6cd1b324bdc85ff882d01a
Kharchenko Nikolay <ithimikat@gmail.com>
2021-02-17 23:22:14 +0200

    add contacts

--- public/pages/kontakty.php
+++ public/pages/kontakty.php
@@ -9,22 +9,41 @@
                 <hr>
                 <address>г. Днепр и область</address>
                 <div>
-                    <img src="public/images/telecom-operators/lifecell.png" alt="Лайф">
+                    <img src="public/images/telecom-operators/lifecell.png" alt="Лайф" title="Лайф">
                     <a href="tel:+380636364260">063 636 4260</a>
                 </div>
                 <div>
-                    <img src="public/images/telecom-operators/kievstar.png" alt="Киевстар">
+                    <img src="public/images/telecom-operators/kievstar.png" alt="Киевстар" title="Киевстар">
                     <a href="tel:+380636364260">067 504 4292</a>
                 </div>
                 <div>
-                    <img src="public/images/telecom-operators/vodafone.png" alt="Водафон">
+                    <img src="public/images/telecom-operators/vodafone.png" alt="Водафон" title="Водафон">
                     <a href="tel:+380636364260">050 684 4905</a>
                 </div>
                 <hr>
-                <h2>Пишите нам на почту</h2>
+                <h2>Вы можете написать нам в чат</h2>
                 <hr>
-                <div style="margin-top: 25px">
-                    <a href="mailto:dneprokol@gmail.com">dneprokol@gmail.com</a>
+            </div>
+            <div class="messengers">
+                <div class="messenger">
+                    <img src="public/images/messengers/viber.png" alt="Viber" title="Viber">
+                    <a href="//chat?380636364260">Написать в Viber</a>
+                </div>
+                <div class="messenger">
+                    <img src="public/images/messengers/telegram.png" alt="Telegram" title="Telegram">
+                    <a href="tg://resolve?domain=kharchenko_nikolay">Написать в Telegram</a>
+                </div>
+                <div class="messenger">
+                    <img src="public/images/messengers/whatsapp.png" alt="Whatsapp" title="Whatsapp">
+                    <a href="https://wa.me/380636364260">Написать в Whatsapp</a>
+                </div>
+            </div>
+            <div class="contacts">
+                <hr>
+                <h2>Почта</h2>
+                <hr>
+                <div>
+                    <a href="mailto:dneprokol@gmail.com" style="margin-top: 20px">dneprokol@gmail.com</a>
                 </div>
             </div>
         </div>
