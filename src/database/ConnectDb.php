ce55ec399fb2b07d972389100db5cc9f6e273b0b
Kharchenko Nikolay <ithimikat@gmail.com>
2021-06-11 11:28:50 +0300

    changed config for connect to database

--- src/database/ConnectDb.php
+++ src/database/ConnectDb.php
@@ -10,14 +10,12 @@ class ConnectDb
     {
         global $configDb;
 
-        $dsn = "mysql:host={$configDb['host']};dbname={$configDb['database']};charset=utf8mb4";
-
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
 
-        $this->pdo = new PDO($dsn, $configDb['user'], $configDb['password'], $options);
+        $this->pdo = new PDO($configDb['dsn'], $configDb['user'], $configDb['password'], $options);
     }
 
     public function getPDO() : PDO
